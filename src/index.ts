598c35ef97875dadd2021637294b89b8151338a4
Your Name <you@example.com>
2024-09-05 16:42:37 -0300

    desabilitando CORS para netlify

--- src/index.ts
+++ src/index.ts
@@ -23,7 +23,7 @@ const io = new SocketIOServer(server, {
     origin: [
       "http://localhost:3000",
       "https://cemear-b549eb196d7c.herokuapp.com",
-      "https://66da07a725d2d96ba6e87ec7--lucky-vacherin-fc35fb.netlify.app",
+      "https://66da0979aa07a97b5c6f1086--sage-jelly-966b0d.netlify.app",
     ],
     methods: ["GET", "POST"],
   },
@@ -48,7 +48,7 @@ const corsOptions = {
   origin: [
     "http://localhost:3000",
     "https://cemear-b549eb196d7c.herokuapp.com",
-    "https://66da07a725d2d96ba6e87ec7--lucky-vacherin-fc35fb.netlify.app",
+    "https://66da0979aa07a97b5c6f1086--sage-jelly-966b0d.netlify.app",
   ],
   methods: ["GET", "POST"],
   allowedHeaders: ["Content-Type", "Authorization"],
