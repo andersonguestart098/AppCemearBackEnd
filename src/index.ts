770137f92b54698f20ab2a13e8cea2ad607eaa95
Your Name <you@example.com>
2024-09-05 16:38:16 -0300

    fix notificações push

--- src/index.ts
+++ src/index.ts
@@ -23,7 +23,7 @@ const io = new SocketIOServer(server, {
     origin: [
       "http://localhost:3000",
       "https://cemear-b549eb196d7c.herokuapp.com",
-      "https://66da07a725d2d96ba6e87ec7--lucky-vacherin-fc35fb.netlify.app/login",
+      "https://66da07a725d2d96ba6e87ec7--lucky-vacherin-fc35fb.netlify.app",
     ],
     methods: ["GET", "POST"],
   },
@@ -48,7 +48,7 @@ const corsOptions = {
   origin: [
     "http://localhost:3000",
     "https://cemear-b549eb196d7c.herokuapp.com",
-    "https://66da07a725d2d96ba6e87ec7--lucky-vacherin-fc35fb.netlify.app/login",
+    "https://66da07a725d2d96ba6e87ec7--lucky-vacherin-fc35fb.netlify.app",
   ],
   methods: ["GET", "POST"],
   allowedHeaders: ["Content-Type", "Authorization"],
